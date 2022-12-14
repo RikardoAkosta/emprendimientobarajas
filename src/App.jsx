c039603fa97c7324b7329834d35a43609f6f918b
RikardoAkosta <richy.andres.acosta@gmail.com>
2022-12-14 13:13:53 +0100

    se hace commit desde portatil

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./pages/auth/Login";
+
+
 
 function App() {
-  return <BrowserRouter>
+  return (<BrowserRouter>
     <Routes>
-
+      <Route path="/" element={<Login />} />
     </Routes>
   </BrowserRouter>
 
 
-
+  )
 
 }
 
