945729871b9171754810a2a5be7d671d15747a07
RikardoAkosta <richy.andres.acosta@gmail.com>
2023-04-10 00:34:02 +0200

    se modifican archivos redme e index[C

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Clients() {
   return <div className='bg-green-500'>
-    <h1>Find us on social media</h1>Clients</div>
-  
+    <h1>Find us on social media</h1>Clients
+    <div>
+      <img src="" alt="logo facebook" />
+    </div>
+  </div>
+
 }
 
 export default Clients
