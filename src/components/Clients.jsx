477a6ac73060763199ed8e52089c6169f46b4138
RikardoAkosta <richy.andres.acosta@gmail.com>
2023-05-25 23:50:58 +0200

    se hace seccion de redes sociales

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 function Clients() {
-  return <div className='bg-green-500'>
-    <h1>Find us on social media</h1>Clients
-    <div>
-      <img src="" alt="logo facebook" />
+  return <div className='bg-white-100 p-8 flex flex-col items-center justify-center gap-8 '>
+    <h1 className='text-2xl font-medium text-gray-800'>Find us on social media</h1>
+    <div className='flex items-center gap-20'>
+      <img src="twiter.png" alt="logo twiter" className="w-40" />
+      <img src="facebook.png" alt="logo twiter" className="w-40" />
+      <img src="instagran.png" alt="logo twiter" className="w-40" />
+      <img src="youtube.png" alt="logo twiter" className="w-40" />
+      <img src="tiktok.png" alt="logo twiter" className="w-40" />
     </div>
   </div>
 
