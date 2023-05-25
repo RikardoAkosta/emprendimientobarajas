9ec63a47ffa4cd53f11241e6c2201394b1916442
RikardoAkosta <richy.andres.acosta@gmail.com>
2023-05-26 00:11:18 +0200

    se crea seccion de trabajos

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Clients() {
-  return <div className='bg-white-100 p-8 flex flex-col items-center justify-center gap-8 '>
-    <h1 className='text-2xl font-medium text-gray-800'>Find us on social media</h1>
-    <div className='flex items-center gap-20'>
+  return <div className='bg-white-100 p-8 flex flex-col items-center justify-center gap-8 mt-20 xl:mt-0'>
+    <h1 className='text-2xl font-medium text-gray-800 text-center'>Find us on social media</h1>
+    <div className='flex flex-col md:flex-row items-center justify-center flex-wrap gap-20'>
       <img src="twiter.png" alt="logo twiter" className="w-40" />
       <img src="facebook.png" alt="logo twiter" className="w-40" />
       <img src="instagran.png" alt="logo twiter" className="w-40" />
---
+++ src/components/Works.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const works = () => {
+  return (
+    <div>works</div>
+  )
+}
